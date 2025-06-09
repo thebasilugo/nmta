d0a28c52fced5f8a86b43b85e50c318fb0a4052a
Basil Ugo <basilugo2@gmail.com>
2025-06-09 21:24:27 +0100

    update archives

---
+++ archives/js/data.js
@@ -0,0 +1,103 @@
+const awardsNews = [
+	{
+		title: "Africa Magic Viewers' Choice Awards (AMVCA) 2025",
+		description:
+			"The most prestigious awards celebrating excellence in African film and television.",
+		image: "https://source.unsplash.com/random/800x600/?awards",
+	},
+	{
+		title: "Nigeria Entertainment Awards (NEA) 2025",
+		description:
+			"Honoring outstanding achievements in Nigerian entertainment, including film, music, and comedy.",
+		image: "https://source.unsplash.com/random/800x600/?cinema",
+	},
+];
+
+const latestStories = [
+	"Nollywood star wins international award at Cannes Film Festival.",
+	"New Nollywood-Hollywood collaboration announced for upcoming blockbuster.",
+	"Nigerian film industry sees record-breaking box office numbers in 2024.",
+];
+
+const upcomingAwards = [
+	{
+		title: "TNI Awards 2025",
+		description:
+			"Join us for a night of celebration as we honor the best in Nollywood. Mark your calendars for August 15, 2025!",
+	},
+	{
+		title: "African Film Awards 2025",
+		description:
+			"Celebrating the best of African cinema. Don't miss this star-studded event on September 30, 2025!",
+	},
+	{
+		title: "Nollywood Excellence Awards 2025",
+		description:
+			"Recognizing outstanding achievements in Nigerian cinema. Save the date: November 5, 2025!",
+	},
+];
+
+const blogPosts = [
+	{
+		title: "The Rise of Nollywood: From Local to Global",
+		description:
+			"Explore how Nigerian cinema has become a global phenomenon, reaching audiences far beyond Africa. From humble beginnings to international recognition, Nollywood's journey is a testament to the power of storytelling and cultural exchange. This article delves into the key factors that have contributed to the industry's growth and its impact on world cinema.",
+		image: "https://source.unsplash.com/random/800x600/?nollywood",
+		author: "Chioma Eze",
+		date: "May 15, 2025",
+	},
+	{
+		title: "Top 10 Nollywood Movies of 2024",
+		description:
+			"Our critics pick the best Nigerian films of the past year, from gripping dramas to hilarious comedies. These films showcase the diversity and creativity of Nollywood, pushing boundaries and setting new standards for African cinema. Discover the must-watch titles that have captivated audiences and critics alike.",
+		image: "https://source.unsplash.com/random/800x600/?movie",
+		author: "Oluwaseun Adebayo",
+		date: "April 30, 2025",
+	},
+	{
+		title: "Interview with Genevieve Nnaji: Nollywood's Global Ambassador",
+		description:
+			"The acclaimed actress and director shares her thoughts on the future of Nigerian cinema. In this exclusive interview, Genevieve Nnaji discusses her journey in the industry, the challenges faced by Nigerian filmmakers, and her vision for Nollywood's continued growth on the international stage. Gain insights from one of the industry's most respected figures.",
+		image: "https://source.unsplash.com/random/800x600/?actor",
+		author: "Emeka Okafor",
+		date: "April 22, 2025",
+	},
+];
+
+const affiliates = [
+	{
+		name: "Nollywood Studios",
+		logo: "https://source.unsplash.com/random/150x75/?logo",
+	},
+	{
+		name: "African Film Festival",
+		logo: "https://source.unsplash.com/random/150x75/?festival",
+	},
+	{
+		name: "Nigerian Film Corporation",
+		logo: "https://source.unsplash.com/random/150x75/?corporation",
+	},
+	{
+		name: "Lagos International Film Festival",
+		logo: "https://source.unsplash.com/random/150x75/?lagos",
+	},
+	{
+		name: "Nollywood Week Paris",
+		logo: "https://source.unsplash.com/random/150x75/?paris",
+	},
+	{
+		name: "Africa Movie Academy Awards",
+		logo: "https://source.unsplash.com/random/150x75/?academy",
+	},
+];
+
+const contactInfo = {
+	address: "123 Nollywood Boulevard, Lagos, Nigeria",
+	email: "info@tni.com",
+	phone: "+234 123 456 7890",
+	socialMedia: {
+		facebook: "https://www.facebook.com/TNI",
+		twitter: "https://www.twitter.com/TNI",
+		instagram: "https://www.instagram.com/TNI",
+	},
+};
