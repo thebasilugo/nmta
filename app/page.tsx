fbdcf90e4f63c1ddfeb9f38cb6a4e75ba49aac86
Basil Ugo <basilugo2@gmail.com>
2025-08-13 11:50:29 +0100

    add sidebar and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,22 @@ export default function HomePage() {
     <main className="min-h-screen">
       <HeroSection />
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 py-16">
-          <div className="lg:col-span-2 space-y-16">
-            <CountdownSection />
-            <AwardCategories />
-            <AboutPreview />
-          </div>
-
-          <div className="lg:col-span-1">
-            <NewsSidebar />
-          </div>
-        </div>
-      </div>
-    </main>
-  )
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				<div className="grid grid-cols-1 lg:grid-cols-3 gap-8 py-16">
+					<div className="lg:col-span-2 space-y-16">
+						<CountdownSection />
+						<AwardCategories />
+						{/* <AboutPreview /> */}
+					</div>
+					<div className="lg:col-span-1">
+						<NewsSidebar />
+						<ShortSidebar />
+					</div>
+					<div className="lg:col-span-3 space-y-16">
+						<AboutPreview />
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
